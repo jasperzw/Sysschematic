51a0dbe37470a448c90951776f35c5a5aac25e22
joller134 <joller134@gmail.com>
2020-03-13 16:01:24 +0100

    Redone the interface so that it has grid structure which makes it easier to add button or other widget/components

--- DeliveryDemo.py
+++ DeliveryDemo.py
@@ -1,62 +1,62 @@
 # Importing tkinter module 
 from tkinter import *
+from exampleLib import *
   
-#een simpele functie waarmee het makkelijker wordt om circkels te maken
+#A simple function to draw circles in a canvas
 def _create_circle(self, x, y, r, **kwargs):
     return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 Canvas.create_circle = _create_circle
 
-def draw_example(w,x,y):
-    w.create_line(100+x, 265+y, 180+x, 265+y, arrow=LAST)
-    w.create_line(220+x, 265+y, 300+x, 265+y, arrow=LAST)
 
-    w.create_line(310+x, 270+y, 310+x, 350+y)
-    w.create_line(310+x, 350+y, 220+x, 350+y, arrow = LAST)
+#initializes the main menu. you have to pass the mainmenu frame and the canvas so that it could pass the canvas id when clearing it
+def initMainMenu(frame, canvas):
 
-    w.create_line(110+x, 275+y, 110+x, 350+y, arrow=FIRST)
-    w.create_line(180+x, 350+y, 110+x, 350+y)
+    #column 0
+    Button(frame, text="load .mat file", height = 1, width=20).grid(row=0, padx=2, pady=2)
+    Button(frame, text="export .mat file", height = 1, width=20).grid(row=1, padx=2, pady=2)
+    Button(frame, text="load example network", command= lambda: draw_example(draw,-10, -150,master), height = 1, width=20).grid(row=2, padx=2, pady=2)
 
-    w.create_line(50+x, 265+y, 100+x, 265+y, arrow = LAST)
-    w.create_line(310+x, 265+y, 350+x, 265+y, arrow = LAST)
+    #column 1
+    Button(frame, text="Options", height = 1, width=20).grid(row=0, column=1, padx=2, pady=2)
+    Button(frame, text="Go to global veiw", height = 1, width=20).grid(row=1, column=1, padx=2, pady=2)
+    Button(frame, text="Clear window", command= lambda: clearWindow(canvas), height = 1, width=20).grid(row=2, column=1, padx=2, pady=2)
 
-    w.create_circle(200+x,270+y,20)
-    w.create_text(200+x,270+y,text="w")
-    w.create_circle(200+x,350+y,20)
-    w.create_text(200+x,350+y,text="w")
+#same as main menu initializes the submenu
+def initSubMenu(frame):
+    Label(frame, text="currently selected:", bg="gray").pack()
+    Button(frame, text="Remove node", height = 1, width=20).pack(padx=2, pady=2)
+    Button(frame, text="View internals", height = 1, width=20).pack(padx=2, pady=2)
+    Button(frame, text="Make known", height = 1, width=20).pack(padx=2, pady=2)
 
-    b1 = Button(master, text = "G1", bg = "cyan", command=lambda : module_call('G1')) 
-    b1_view = w.create_window(110+x, 265+y, window=b1)
-
-    b2 = Button(master, text = "G2", bg = "cyan", command=lambda : module_call('G2')) 
-    b2_view = w.create_window(310+x, 265+y, window=b2)
-
-def initialize_display(w):
-    m1 = Button(master, text = "Load .mat file") 
-    m1_view = w.create_window(50,20, window=m1)
-    m2 = Button(master, text = "Load example", command=lambda : draw_example(w,300,200) )
-    m2_view = w.create_window(50,50, window=m2)
-    m3 = Button(master, text = "Clear canvas", command=lambda : clear_canvas(w))
-    m3_view = w.create_window(50,80, window=m3)
-    w.create_text(900,10,text="The current value is:")
-
-def module_call(module_name):
-    w.create_text(965,10,text=module_name)
-
-def clear_canvas(w):
-    w.delete('all')
-    initialize_display(w)
+def clearWindow(canvas):
+    canvas.delete("all")
 
 # creating Tk window 
 master = Tk() 
+master.configure(background="gray")
   
-# setting geometry of tk window 
 
+#seperating the menu in different frames which will hold all the components so that it is easier to use .grid for button placemant
 master.title("Delivery Demo")
+mainMenu = Frame(master, bg="gray")
+canvas = Frame(master, bg="white")
+subMenu = Frame(master, bg="gray")
+
+#alligning the grid
+mainMenu.grid(row=0, sticky=NW)
+canvas.grid(row=1)
+subMenu.grid(row=0, column=1, rowspan=2, sticky=NE)
+
+draw = Canvas(canvas, height=300, width=500, bg="white")
+draw.pack()
+initMainMenu(mainMenu, draw)
+initSubMenu(subMenu)
+
 
 # canvas aanmaken en even een proef of concept hardcode op het scherm
-w = Canvas(master, width=1000, height=600)
-w.pack()
-initialize_display(w)
+#w = Canvas(master, width=1000, height=600)
+#w.pack()
+#initialize_display(w,master)
 
   
 # label widget 
