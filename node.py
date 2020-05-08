4adea788a6ebd916963ae81d4335cc5b341baf97
joller134 <joller134@gmail.com>
2020-05-08 19:39:59 +0200

    adding node functions to seperate file

--- node.py
+++ node.py
@@ -1,84 +1,34 @@
+def removeNodeCall(draw,master,number_of_nodes,btnStore,lineStore,lineNumber):
 
-#from var import lineNumber, lineStore, number_of_nodes, btnStore
-#from tkinter import *
+    node = 0
+    #removing last node
+    #check if their is atleast 1 node
+    if(number_of_nodes>0):
+        #update the number of nodes
+        for x in range(number_of_nodes):
+            if(btnStore[x]!=0):
+                #check the colors the find the specific transfer
+                if(btnStore[x][1].cget('bg')=="lime"):
+                    #print(b_view[x])
+                    #print(b_view)
+                    #print(x)
+                    node = btnStore[x][1]
+                    #delete it from the canvas and set it to 0 in btnStore
+                    draw.delete(btnStore[x][0])
+                    btnStore[x] = 0
+                    if(x==number_of_nodes-1):
+                        number_of_nodes = number_of_nodes - 1
+                    #print(number_of_nodes)
+                    #print(b_view)
 
-def selectNode(node):
-    global btnStore
-    global number_of_nodes
-
-    print("the node which is to be selected", node)
-    if(btnStore[node][1]["bg"]=="cyan"):
-        btnStore[node][1]["bg"]="lime"
-    else:
-        btnStore[node][1]["bg"]="cyan"
-
-# adding a node
-def addNode(w,x,y,master):
-        global number_of_nodes
-        global btnStore
-        node = 0
-        #creating node x
-
-        if(number_of_nodes==0):
-            btn = Button(master, text = "G"+str(number_of_nodes), command = lambda: selectNode(0) , bg = "cyan")
-            save = [w.create_window(x, y, window=btn),btn,x,y]
-            btnStore.append(save)
-            number_of_nodes = number_of_nodes + 1
-            print("start initial node")
-
-        else:
-            for m in range(number_of_nodes-1):
-                if(btnStore[m]==0):
-                    node = m
-                    btn = Button(master, text = "G"+str(node), command = lambda: selectNode(node) , bg = "cyan")
-                    save = [w.create_window(x, y, window=btn),btn,x,y]
-                    btnStore[m] = save
-                    print("added node in existing place")
-
-            if(number_of_nodes!=0 and node == 0):
-                temp = number_of_nodes
-                btn = Button(master, text = "G"+str(number_of_nodes), command = lambda: selectNode(temp) , bg = "cyan")
-                save = [w.create_window(x, y, window=btn),btn,x,y]
-                btnStore.append(save)
-                number_of_nodes = number_of_nodes + 1
-                print("appended node to back of list")
-
-        print(btnStore)
-
-def removeNode(w, master):
-        global number_of_nodes
-        global btnStore
-        global lineStore
-        global lineNumber
-
-        node = 0
-        #removing last node
-        if(number_of_nodes>0):
-            #update the number of nodes
-            for x in range(number_of_nodes-1):
-                if(btnStore[x]!=0):
-                    if(btnStore[x][1].cget('bg')=="lime"):
-                        #print(b_view[x])
-                        #print(b_view)
-                        #print(x)
-                        node = x
-                        w.delete(btnStore[x][0])
-                        btnStore[x] = 0
-                        if(x==number_of_nodes-1):
-                            number_of_nodes = number_of_nodes - 1
-                        #print(number_of_nodes)
-                        #print(b_view)
-
-
-            print("starting deleting lines at node=",node,"")
-            for x in range(lineNumber):
-                print("x: ",x," lineStore:",lineStore[x],"")
-                if(lineStore[x]!=0):
-                    if(lineStore[x][1]==node or lineStore[x][2] == node):
-                        print("x: ",x," is deleted")
-                        w.delete(lineStore[x][0])
-                        lineStore[x]=0
-
-#            draw.delete(lineStore[temp-1][0])
-#            lineStore[temp-1]=0
-#            removeNode(draw,master)
+        #search if we can find it in lineStore and remove it
+        print("starting deleting lines at node=",node,"")
+        for x in range(lineNumber):
+            print("x: ",x," lineStore:",lineStore[x],"")
+            if(lineStore[x]!=0):
+                if(lineStore[x][3] ==  node):
+                    print("x: ",x," is deleted")
+                    draw.delete(lineStore[x][0])
+                    lineStore[x]=0
+    
+    return number_of_nodes, btnStore, lineStore, lineNumber
