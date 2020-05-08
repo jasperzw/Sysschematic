00aaf7b1e5212d2299a9ac399734f91f8ee7e348
joller134 <joller134@gmail.com>
2020-05-08 19:39:59 +0200

    adding node functions to seperate file

--- DeliveryDemo.py
+++ DeliveryDemo.py
@@ -5,6 +5,7 @@ from matImport import readFile
 from tkinter.filedialog import askopenfilename
 import math
 from Scrollwindow import *
+from node import removeNodeCall
 
 
 
@@ -237,41 +238,13 @@ def addNode(w,x,y,master):
         print(btnStore)
 
 def removeNode(w, master):
-        global number_of_nodes
-        global btnStore
-        global lineStore
-        global lineNumber
+    global number_of_nodes
+    global btnStore
+    global lineStore
+    global lineNumber
 
-        node = 0
-        #removing last node
-        #check if their is atleast 1 node
-        if(number_of_nodes>0):
-            #update the number of nodes
-            for x in range(number_of_nodes):
-                if(btnStore[x]!=0):
-                    #check the colors the find the specific transfer
-                    if(btnStore[x][1].cget('bg')=="lime"):
-                        #print(b_view[x])
-                        #print(b_view)
-                        #print(x)
-                        node = btnStore[x][1]
-                        #delete it from the canvas and set it to 0 in btnStore
-                        w.delete(btnStore[x][0])
-                        btnStore[x] = 0
-                        if(x==number_of_nodes-1):
-                            number_of_nodes = number_of_nodes - 1
-                        #print(number_of_nodes)
-                        #print(b_view)
+    number_of_nodes, btnStore, lineStore, lineNumber = removeNodeCall(draw,master,number_of_nodes,btnStore,lineStore,lineNumber)
 
-            #search if we can find it in lineStore and remove it
-            print("starting deleting lines at node=",node,"")
-            for x in range(lineNumber):
-                print("x: ",x," lineStore:",lineStore[x],"")
-                if(lineStore[x]!=0):
-                    if(lineStore[x][3] ==  node):
-                        print("x: ",x," is deleted")
-                        w.delete(lineStore[x][0])
-                        lineStore[x]=0
 
 """
 below are the functions regarding
