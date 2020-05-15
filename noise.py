3cc5c6686476496b2f37d6161db2986cb15a3093
Notkamp <g.e.g.notkamp@student.tue.nl>
2020-05-16 00:04:45 +0200

    ff logische naampjes

--- noise.py
+++ noise.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-def addNoiseNodeCall(draw, x, y, master, outputNumber, outputStore, img1Btn):
+def addNoiseNodeCall(draw, x, y, master, noiseNodeNumber, noiseNodeStore, img1Btn):
     #create output
     switch = 0
     node = 0
@@ -18,29 +18,29 @@ def addNoiseNodeCall(draw, x, y, master, outputNumber, outputStore, img1Btn):
     save = [draw.create_window(x, y, window=img1Btn),img1Btn,x,y,draw.create_window(x+10,y+5,window=nmb),nmb]
 
     #earch if their is a empty entry.
-    for m in range(outputNumber):
-        if(outputStore[m]==0):
+    for m in range(noiseNodeNumber):
+        if(noiseNodeStore[m]==0):
             node=m
             nmb.configure(text=str(m+1))
             save[1].nmb = m
-            outputStore[m] = save
+            noiseNodeStore[m] = save
             img1Btn.image.text = str(m+1)
             switch = 1
 
             #initial output
-    if(outputNumber==0):
-        outputStore.append(save)
-        outputNumber = outputNumber + 1
+    if(noiseNodeNumber==0):
+        noiseNodeStore.append(save)
+        noiseNodeNumber = noiseNodeNumber + 1
         switch = 1
 
     #append if no empty entry
     if(switch==0):
-        nmb.configure(text=str(outputNumber+1))
-        save[1].nmb=outputNumber
-        outputStore.append(save)
-        outputNumber = outputNumber + 1
+        nmb.configure(text=str(noiseNodeNumber+1))
+        save[1].nmb=noiseNodeNumber
+        noiseNodeStore.append(save)
+        noiseNodeNumber = noiseNodeNumber + 1
 
-    return outputNumber, outputStore
+    return noiseNodeNumber, noiseNodeStore
 
 def selectNoiseNodeCall(draw, master, noiseNodeNumber, noiseNodeStore, currentAmountOutputSelected, id):
         #each output has a stat variable which indicates state. stat == 1 is not selected, stat == 2 is selected
@@ -75,22 +75,22 @@ def selectNoiseNodeCall(draw, master, noiseNodeNumber, noiseNodeStore, currentAm
 
         return currentAmountOutputSelected
 
-def removeNoiseNodeCall(outputStore, outputNumber, lineStore, lineNumber, draw):
+def removeNoiseNodeCall(noiseNodeStore, noiseNodeNumber, lineStore, lineNumber, draw):
     #search for output and set it to 0
 
-    for x in range(outputNumber):
-        if(outputStore[x]!=0):
-            if(outputStore[x][1].stat == 2):
+    for x in range(noiseNodeNumber):
+        if(noiseNodeStore[x]!=0):
+            if(noiseNodeStore[x][1].stat == 2):
                 for i in range(lineNumber):
                     print("i: ",i," lineStore:",lineStore[i],"")
                     if(lineStore[i]!=0):
-                        if(lineStore[i][1]==outputStore[x][1] or lineStore[i][2]==outputStore[x][1]):
+                        if(lineStore[i][1]==noiseNodeStore[x][1] or lineStore[i][2]==noiseNodeStore[x][1]):
                             print("i: ",i," is deleted")
                             lineStore[i][3]["bg"]="lime"
                             draw.delete(lineStore[i][0])
                             #removeNode(draw, master)
                             lineStore[i]=0
-                draw.delete(outputStore[x][4])
-                draw.delete(outputStore[x][0])
-                outputStore[x] = 0
-    return outputStore, lineStore
+                draw.delete(noiseNodeStore[x][4])
+                draw.delete(noiseNodeStore[x][0])
+                noiseNodeStore[x] = 0
+    return noiseNodeStore, lineStore
